d21a63bb7a6eb2a408d9c359997b78fe946d3df5
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2022-11-26 19:02:34 -0600

    Step 1 complete
    First function created.

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget(){
+    let number = Math.floor(Math.random() * 9);
+    return number;
+}
+
+console.log(generateTarget());
+
+
